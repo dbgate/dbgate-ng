7c1e138fe8d459f8cbefc243ff2f2d73012b6a98
Jan Prochazka <jan.prochazka@sprinx.cz>
2025-08-07 16:33:07 +0200

    typings

---
+++ packages/types/api/archive-api.d.ts
@@ -0,0 +1,20 @@
+export type Archive_Folders_Request = {
+  controller: "archive";
+  action: "folders";
+  params: {};
+  response: {
+    name: string;
+    type: "jsonl" | "archive";
+  }[];
+};
+
+export type Archive_CreateFolder_Request = {
+  controller: "archive";
+  action: "createFolder";
+  params: {
+    folder: string;
+  };
+  response: boolean;
+};
+
+export type Archive_Request = Archive_Folders_Request | Archive_CreateFolder_Request;
