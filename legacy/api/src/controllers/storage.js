b4b9ce44453235a5b2025a2e7a78415d16f6e0fe
Jan Prochazka <jan.prochazka@sprinx.cz>
2025-08-07 16:23:52 +0200

    added legacy version

---
+++ legacy/api/src/controllers/storage.js
@@ -0,0 +1,44 @@
+module.exports = {
+  connections_meta: true,
+  async connections(req) {
+    return null;
+  },
+
+  async getExportedDatabase() {
+    return {};
+  },
+
+  getConnection_meta: true,
+  async getConnection({ conid }) {
+    return null;
+  },
+
+  async loadSuperadminPermissions() {
+    return [];
+  },
+
+  getConnectionsForLoginPage_meta: true,
+  async getConnectionsForLoginPage() {
+    return null;
+  },
+
+  getStorageConnectionError() {
+    return null;
+  },
+
+  readConfig_meta: true,
+  async readConfig({ group }) {
+    return {};
+  },
+
+  sendAuditLog_meta: true,
+  async sendAuditLog({}) {
+    return null;
+  },
+
+  startRefreshLicense() {},
+
+  async getUsedEngines() {
+    return null;
+  },
+};
